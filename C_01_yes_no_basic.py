2d26d54b067032463343cbf906464d01e63902e8
dilshan3110 <nagrad3134@masseyhigh.school.nz>
2025-02-11 14:47:03 +1300

    Add files via upload

---
+++ C_01_yes_no_basic.py
@@ -0,0 +1,17 @@
+
+while True:
+
+    want_instructions = input("Do you want to see the instructions? ").lower()
+
+    # check the user says yes / no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you said yes")
+        break
+    elif want_instructions == "no" or want_instructions == "y":
+        print("you said no")
+        break
+    else:
+        print("please enter yes / no")
+        continue
+
+print("we are done")
